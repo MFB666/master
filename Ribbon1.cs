2ac6ce7ec841f90fa2d92256ea2cce8fc7c6196c
MFB666 <1033481995@qq.com>
2021-08-23 18:19:39 +0800

    20210823First Commit

---
+++ Ribbon1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace HandlePptHexin
+{
+    public partial class Ribbon1
+    {
+        PowerPoint.Application app;
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            app = Globals.ThisAddIn.Application;
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Hello World");
+        }
+    }
+}
